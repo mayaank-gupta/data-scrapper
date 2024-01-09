87f86cc58758c7cc57ee2e2c4193f64e529ef419
nikettongare <nikettongaren24@gmail.com>
2024-01-09 10:57:40 +0530

    changes

--- fetch-history.js
+++ fetch-history.js
@@ -1,9 +1,9 @@
-const puppeteer = require("puppeteer");
-const moment = require("moment-timezone");
-const axios = require("axios");
-const sendMessage = require("./send_message");
-const fs = require("fs");
-const filePath = "fetch-history.json";
+const puppeteer = require('puppeteer');
+const moment = require('moment-timezone');
+const axios = require('axios');
+const sendMessage = require('./send_message');
+const fs = require('fs');
+const filePath = 'fetch-history.json';
 
 let browser;
 async function launchBrowser() {
@@ -17,7 +17,7 @@ const stockUrl = `https://stock-daily-price.vercel.app/get_stock_data`;
 
 async function fetchData(scanners) {
   try {
-    console.log("fetchHistory Executed!");
+    console.log('fetchHistory Executed!');
     if (Array.isArray(scanners) && scanners.length) {
       await launchBrowser();
       for (let scanner of scanners) {
@@ -25,24 +25,34 @@ async function fetchData(scanners) {
         let finalObject = [];
         let addedElements = [];
         // Intercept and log responses
-        page.on("response", async (response) => {
-          if (response.url().includes("backtest/process")) {
+        page.on('response', async (response) => {
+          if (response.url().includes('backtest/process')) {
             response
               .text()
               .then(async (body) => {
                 const parsedJson = JSON.parse(body);
-                if (Array.isArray(parsedJson.metaData) && parsedJson.metaData.length) {
-                  if (parsedJson.metaData[0]["tradeTimes"].length >= 20) {
-                    const lastResponseElement = parsedJson.aggregatedStockList[parsedJson.aggregatedStockList.length - 1].filter(
-                      (el, j) => j % 3 === 0
+                if (
+                  Array.isArray(parsedJson.metaData) &&
+                  parsedJson.metaData.length
+                ) {
+                  if (parsedJson.metaData[0]['tradeTimes'].length >= 20) {
+                    const lastResponseElement = parsedJson.aggregatedStockList[
+                      parsedJson.aggregatedStockList.length - 1
+                    ].filter((el, j) => j % 3 === 0);
+                    let last20Responses = parsedJson.aggregatedStockList.slice(
+                      -21,
+                      -1
                     );
-                    let last20Responses = parsedJson.aggregatedStockList.slice(-21, -1);
-                    finalObject = last20Responses.flatMap((element, i) => last20Responses[i].filter((el, j) => j % 3 === 0));
+                    finalObject = last20Responses.flatMap((element, i) =>
+                      last20Responses[i].filter((el, j) => j % 3 === 0)
+                    );
                     finalObject = [...new Set(finalObject)];
-                    addedElements = lastResponseElement.filter((item) => !finalObject.includes(item));
+                    addedElements = lastResponseElement.filter(
+                      (item) => !finalObject.includes(item)
+                    );
                     if (addedElements.length) {
                       axios({
-                        method: "post",
+                        method: 'post',
                         url: stockUrl,
                         data: {
                           symbols: addedElements.map((el) => `${el}.NS`),
@@ -52,13 +62,19 @@ async function fetchData(scanners) {
                           let formattedMessage = [];
                           addedElements.forEach((el) => {
                             if (Object.keys(response.data).includes(el)) {
-                              formattedMessage.push([`\n${el} >>> ${response.data[el]["close"]}`]);
+                              formattedMessage.push([
+                                `\n${el} >>> ${response.data[el]['close']}`,
+                              ]);
                             }
                           });
 
-                          const message = `<b>${scanners[scanner.id].name}</b>\n\n<b>New Added:</b> <i>${formattedMessage.join(
-                            ""
-                          )}</i>\n\n<b>Time:</b> <i>${moment().utcOffset("+05:30").format("YYYY-MM-DD HH:mm A")}</i>\n`;
+                          const message = `<b>${
+                            scanners[scanner.id].name
+                          }</b>\n\n<b>New Added:</b> <i>${formattedMessage.join(
+                            ''
+                          )}</i>\n\n<b>Time:</b> <i>${moment()
+                            .utcOffset('+05:30')
+                            .format('YYYY-MM-DD HH:mm A')}</i>\n`;
                           await sendMessage(message);
                         }
                       });
@@ -69,19 +85,20 @@ async function fetchData(scanners) {
               .catch((err) => console.error(err));
           }
         });
-        page.on("error", (error) => {
-          console.error("Page error:", error);
+        page.on('error', (error) => {
+          console.error('Page error:', error);
         });
         await page.goto(scanner.url, {
-          waitUntil: "load",
+          waitUntil: 'load',
         });
         await new Promise((r) => setTimeout(r, 10000));
         await page.close();
       }
-      await browser.close();
     }
   } catch (err) {
     console.log(err);
+  } finally {
+    await browser.close();
   }
 }
 module.exports = fetchData;
--- runner.js
+++ runner.js
@@ -1,68 +1,96 @@
-const puppeteer = require("puppeteer");
-const moment = require("moment");
-const sendMessage = require("./send_message");
-const fs = require("fs");
-const filePath = "scanners.json";
+const puppeteer = require('puppeteer');
+const moment = require('moment');
+const sendMessage = require('./send_message');
+const fs = require('fs');
+const filePath = 'scanners.json';
 
 function arraysEqual(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((value, index) => value === arr2[index]);
+  return (
+    arr1.length === arr2.length &&
+    arr1.every((value, index) => value === arr2[index])
+  );
 }
 let browser;
 async function launchBrowser() {
   browser = await puppeteer.launch({
-    headless: "new",
+    headless: 'new',
     timeout: 10000,
   });
 }
 
 async function fetchData(scanners) {
   try {
-    console.log("fetchData Executed!");
+    console.log('fetchData Executed!');
     if (Array.isArray(scanners) && scanners.length) {
       await launchBrowser();
       for (let scanner of scanners) {
         const page = await browser.newPage();
 
         // Intercept and log responses
-        page.on("response", (response) => {
-          if (response.url().includes("screener/process")) {
+        page.on('response', (response) => {
+          if (response.url().includes('screener/process')) {
             response.text().then(async (body) => {
               const parsedJson = JSON.parse(body);
               if (Array.isArray(parsedJson.data) && parsedJson.data.length) {
                 const extractedData = parsedJson.data.map((el) => el.nsecode);
-                const modifiedArray = parsedJson.data.map((el) => [el.nsecode, el.close]);
+                const modifiedArray = parsedJson.data.map((el) => [
+                  el.nsecode,
+                  el.close,
+                ]);
                 if (!scanners[scanner.id].old_response.length) {
                   scanners[scanner.id].old_response = extractedData;
-                  const message = `<b>${scanners[scanner.id].name}</b>\n\n<b>Stocks:</b> ${modifiedArray
+                  const message = `<b>${
+                    scanners[scanner.id].name
+                  }</b>\n\n<b>Stocks:</b> ${modifiedArray
                     .map((item) => `\n- ${item[0]} --> ${item[1]}`)
-                    .join("")}\n\n<b>Total Stocks: ${extractedData.length}</b>\n\n<b>Time:</b> ${moment()
-                    .utcOffset("+05:30")
-                    .format("YYYY-MM-DD HH:mm A")}\n`;
+                    .join('')}\n\n<b>Total Stocks: ${
+                    extractedData.length
+                  }</b>\n\n<b>Time:</b> ${moment()
+                    .utcOffset('+05:30')
+                    .format('YYYY-MM-DD HH:mm A')}\n`;
                   await sendMessage(message);
-                } else if (arraysEqual(scanners[scanner.id].old_response, extractedData)) {
+                } else if (
+                  arraysEqual(scanners[scanner.id].old_response, extractedData)
+                ) {
                 } else {
                   scanners[scanner.id].latest_response = extractedData;
-                  const addedElements = scanners[scanner.id].latest_response.filter(
+                  const addedElements = scanners[
+                    scanner.id
+                  ].latest_response.filter(
                     (item) => !scanners[scanner.id].old_response.includes(item)
                   );
-                  const removedElements = scanners[scanner.id].old_response.filter(
-                    (item) => !scanners[scanner.id].latest_response.includes(item)
+                  const removedElements = scanners[
+                    scanner.id
+                  ].old_response.filter(
+                    (item) =>
+                      !scanners[scanner.id].latest_response.includes(item)
                   );
                   scanner.old_response = extractedData;
-                  const message = `<b>${scanners[scanner.id].name}</b>\n\n<b>New Added:</b> <i>${addedElements.join(
-                    ", "
-                  )}</i>\n\n<b>Removed:</b> <i>${removedElements.join(", ")}</i>\n\n<b>Stocks:</b> ${modifiedArray
+                  const message = `<b>${
+                    scanners[scanner.id].name
+                  }</b>\n\n<b>New Added:</b> <i>${addedElements.join(
+                    ', '
+                  )}</i>\n\n<b>Removed:</b> <i>${removedElements.join(
+                    ', '
+                  )}</i>\n\n<b>Stocks:</b> ${modifiedArray
                     .map((item) => `\n- ${item[0]} --> ${item[1]}`)
-                    .join("")}\n\n<b>Total Stocks: ${extractedData.length}</b>\n\n<b>Time:</b> <i>${moment()
-                    .utcOffset("+05:30")
-                    .format("YYYY-MM-DD HH:mm A")}</i>\n`;
+                    .join('')}\n\n<b>Total Stocks: ${
+                    extractedData.length
+                  }</b>\n\n<b>Time:</b> <i>${moment()
+                    .utcOffset('+05:30')
+                    .format('YYYY-MM-DD HH:mm A')}</i>\n`;
                   await sendMessage(message);
                 }
-                fs.writeFile(filePath, JSON.stringify(scanners), "utf8", (writeErr) => {
-                  if (writeErr) {
-                    console.error("Error writing to file:", writeErr);
+                fs.writeFile(
+                  filePath,
+                  JSON.stringify(scanners),
+                  'utf8',
+                  (writeErr) => {
+                    if (writeErr) {
+                      console.error('Error writing to file:', writeErr);
+                    }
                   }
-                });
+                );
               }
             });
           }
@@ -71,12 +99,13 @@ async function fetchData(scanners) {
         // Navigate to the webpage
         await page.goto(scanner.url);
 
-        // Close the browser
-        await browser.close();
+        await page.close();
       }
     }
   } catch (err) {
     console.log(err);
+  } finally {
+    await browser.close();
   }
 }
 module.exports = fetchData;
