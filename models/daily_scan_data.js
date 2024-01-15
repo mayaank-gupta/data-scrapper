176e71e9e12fa6659c4fd295637fcbdb0c8af5cd
xMayank <mayankgupta.work@gmail.com>
2024-01-15 11:39:25 +0530

    models updated

--- models/daily_scan_data.js
+++ models/daily_scan_data.js
@@ -1,42 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
   const daily_scan_data = sequelize.define(
-    'daily_scan_data',
+    "daily_scan_data",
     {
       id: {
         allowNull: false,
         primaryKey: true,
         autoIncrement: true,
         type: DataTypes.INTEGER,
-        field: 'id',
+        field: "id",
       },
       scannerId: {
         allowNull: false,
         type: DataTypes.INTEGER,
-        field: 'scanner_id',
+        field: "scanner_id",
       },
       tickerList: {
         allowNull: false,
         type: DataTypes.JSON,
-        field: 'ticker_list',
+        field: "ticker_list",
       },
       createdDate: {
         allowNull: false,
         type: DataTypes.DATE,
-        field: 'created_at',
-        defaultValue: new Date(),
+        field: "created_at",
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updatedDate: {
         allowNull: false,
         type: DataTypes.DATE,
-        field: 'updated_at',
-        defaultValue: new Date(),
+        field: "updated_at",
+        defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
       },
     },
     {
       freezeTableName: true,
       timestamps: true,
-      createdAt: 'createdDate',
-      updatedAt: 'updatedDate',
+      createdAt: "createdDate",
+      updatedAt: "updatedDate",
     }
   );
   daily_scan_data.associate = function () {
