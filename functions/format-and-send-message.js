e694cbfe4c34d36c9689cee5fd293828126846bf
xMayank <mayankgupta.work@gmail.com>
2024-02-01 10:51:05 +0530

    changed done

--- functions/format-and-send-message.js
+++ functions/format-and-send-message.js
@@ -1,20 +1,31 @@
-const moment = require('moment-timezone');
-const sendMessage = require('../send_message');
+const moment = require("moment-timezone");
+const sendMessage = require("../send_message");
+
+function calculateStocks(stockPrice, investmentAmount) {
+  if (typeof stockPrice !== "number" || typeof investmentAmount !== "number" || stockPrice <= 0 || investmentAmount <= 0) {
+    return "Invalid input. Please provide valid positive numbers.";
+  }
+
+  if (investmentAmount <= stockPrice) {
+    return 1;
+  }
+
+  const numberOfStocks = Math.floor(investmentAmount / stockPrice);
+  return numberOfStocks;
+}
 
 async function formatAndSendMessage(scannerName, newElements) {
-  if (!newElements ||(newElements && !newElements.length)) {
+  if (!newElements || (newElements && !newElements.length)) {
     return;
   }
 
   newElements = newElements.map((stock) => {
-    return `<b>${stock.name}</b> >>> ${stock.price}`;
+    return `<b>${stock.name}</b> >>> ${stock.price} | StocksCount: ${calculateStocks(stock.price, 3000)}`;
   });
 
-  const message = `<b>${scannerName}</b>\n\n<b>New Added:</b>\n<i>${newElements.join(
-    '\n'
-  )}</i>\n\n<b>Time:</b> <i>${moment()
-    .utcOffset('+05:30')
-    .format('YYYY-MM-DD HH:mm A')}</i>\n`;
+  const message = `<b>${scannerName}</b>\n\n<b>New Added:</b>\n<i>${newElements.join("\n")}</i>\n\n<b>Time:</b> <i>${moment()
+    .utcOffset("+05:30")
+    .format("YYYY-MM-DD HH:mm A")}</i>\n`;
 
   return await sendMessage(message);
 }
