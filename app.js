e6748ceb8afd8ea04946fc75d5283276900eb792
xMayank <mayankgupta.work@gmail.com>
2024-02-01 10:19:23 +0530

    changed done

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const job = CronJob.from({
 
 
 const fetchScannerData = CronJob.from({
-  cronTime: "* 9-16 * * 1-5",
+  cronTime: "*/5 9-16 * * 1-5",
   onTick: function () {
     fetchScannersData(fetchHistoryScanners);
   },
