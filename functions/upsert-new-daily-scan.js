5c463e0b5eaee154428dcd40bfc856734da00f66
nikettongare <nikettongaren24@gmail.com>
2024-02-01 09:59:57 +0530

    ads

--- functions/upsert-new-daily-scan.js
+++ functions/upsert-new-daily-scan.js
@@ -8,7 +8,7 @@ async function upsertNewDailyScan(scannerId, tickerList) {
     return {
       name: item.symbol,
       price: item.price,
-      time: item.time,
+      time: item.time ? item.time : "" ,
     };
   });
 
